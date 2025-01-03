34489a676ff3b7ba1ec90d5e3344cc080e5e13ab
lokeshnandan <lokeshnandan007@gmail.com>
2025-01-03 17:00:40 +0530

    temp commit

--- src/app/(user)/business/[brand]/page.tsx
+++ src/app/(user)/business/[brand]/page.tsx
@@ -1,23 +1,15 @@
 "use client";
-import { animateCards } from "@/lib/animation";
+
 import { Metadata } from "next";
 import React, { useEffect, useRef } from "react";
 import { BrandHeader } from "./components/BrandPageBanner";
 import { brandInfo, dummyCategoryData, posts, reviews } from "./data/mockdata";
-import { ReviewCard } from "./components/review-card";
 import { PostCard } from "./components/post-card";
 import { Footer } from "@/components/layout";
 import SingleCategoryShorts from "./components/postSection";
 
 export default function BrandPage({ params }: { params: any }) {
-  const resolvedParams = React.useMemo(() => params, [params]);
-  const contentRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    if (contentRef.current) {
-      animateCards(contentRef.current);
-    }
-  }, []);
 
   // Fetch brand data (replace this with API call)
   //   const brandData = {
@@ -30,9 +22,9 @@ export default function BrandPage({ params }: { params: any }) {
       <BrandHeader info={brandInfo} />
 
       {/* product review section */}
-      <div className="container">
+      {/* <div className="container">
         <SingleCategoryShorts categoryData={dummyCategoryData} />
-      </div>
+      </div> */}
 
       {/* post section */}
 
